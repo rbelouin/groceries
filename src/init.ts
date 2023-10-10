d0744facb070230631f79f9cb72d834c4ce609cc
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2023-10-10 22:56:42 +0200

    Create the "Generated List" sheet and corresponding trigger

--- src/init.ts
+++ src/init.ts
@@ -3,9 +3,14 @@ const LIST_SHEET_TITLE = "Liste de courses";
 const LIST_SHEET_ARTICLE_NAME = "Nom";
 const LIST_SHEET_ARTICLE_QUANTITY = "Quantité";
 
+const GENERATED_LIST_SHEET_NAME = "Liste générée";
+const GENERATED_LIST_SHEET_TITLE = "Liste générée";
+
 function init() {
   const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   createListSheet(spreadsheet);
+  createGeneratedListSheet(spreadsheet);
+  createTrigger(spreadsheet);
 }
 
 function createListSheet(spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet) {
@@ -13,6 +18,42 @@ function createListSheet(spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet) 
 
   const sheet = spreadsheet.insertSheet(LIST_SHEET_NAME, 0);
   createHeader(sheet, LIST_SHEET_TITLE);
+
+  sheet.getRange("A2")
+    .setValue(LIST_SHEET_ARTICLE_NAME)
+    .setFontStyle("italic");
+
+  sheet.getRange("B2")
+    .setValue(LIST_SHEET_ARTICLE_QUANTITY)
+    .setFontStyle("italic");
+}
+
+function createGeneratedListSheet(spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet) {
+  if (spreadsheet.getSheetByName(GENERATED_LIST_SHEET_NAME)) return;
+
+  const sheet = spreadsheet.insertSheet(GENERATED_LIST_SHEET_NAME, 1);
+  createHeader(sheet, GENERATED_LIST_SHEET_TITLE);
+
+  sheet.getRange("A3:A")
+    .insertCheckboxes();
+
+  sheet.getRange("B2")
+    .setValue(LIST_SHEET_ARTICLE_NAME)
+    .setFontStyle("italic");
+
+  sheet.getRange("C2")
+    .setValue(LIST_SHEET_ARTICLE_QUANTITY)
+    .setFontStyle("italic");
+}
+
+function createTrigger(spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet) {
+  const triggers = ScriptApp.getUserTriggers(spreadsheet);
+  if (triggers.length !== 0) return;
+
+  ScriptApp.newTrigger("onChange")
+    .forSpreadsheet(spreadsheet)
+    .onChange()
+    .create();
 }
 
 function createHeader(sheet: GoogleAppsScript.Spreadsheet.Sheet, title: string) {
@@ -26,12 +67,4 @@ function createHeader(sheet: GoogleAppsScript.Spreadsheet.Sheet, title: string) 
   sheet.getRange("A1:2")
     .setBackground("#0f6b4f")
     .setFontColor("#ffffff");
-
-  sheet.getRange("A2")
-    .setValue(LIST_SHEET_ARTICLE_NAME)
-    .setFontStyle("italic");
-
-  sheet.getRange("B2")
-    .setValue(LIST_SHEET_ARTICLE_QUANTITY)
-    .setFontStyle("italic");
 }
---
+++ src/onChange.ts
@@ -0,0 +1,3 @@
+function onChange() {
+  console.log("Hello, world!");
+}
